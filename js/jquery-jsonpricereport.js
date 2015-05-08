932e9dfeb7c99b32ae8ce6273969b4869feaec25
ouchi <ouchi@itrade.co.jp>
2015-05-08 13:15:52 +0900

    少し修正

--- js/jquery-jsonpricereport.js
+++ js/jquery-jsonpricereport.js
@@ -1,93 +1,57 @@
-
-$(function() {
-$('li#price-report55').jsonpricereport({
-        commodity: '55'
-    })
-$('li#price-report50').jsonpricereport({
-        commodity: '50'
-    })
+$(function () {
+    $('ul#collapsePrice > li').jsonpricereport();
 });
 
-// ----------------------------------------
-// 相場表テーブル json読み込み プラグイン
-// ----------------------------------------
-// usage: 
-// $('#price-report').jsonpricereport({
-//   jsonfile: './price_rice.json'
-// });
-// ----------------------------------------
+(function ($) {
+    $.fn.jsonpricereport = function () {
 
-(function($){
-    // 最新
-    $.fn.jsonpricereport = function(options){
-
-        var args = $.extend({
-            commodity: null
-        },options || {});
-
-        if( !args.commodity ) {
-            return false; // 引数不足
-        }
-
-        $(this).each(function(){
+        $(this).each(function () {
             var self = $(this);
-            var jsonfile = './price_' + args.commodity + '.json';
-
-            var timestamp = '?timestamp='+(new Date()).getTime(); // IEのajaxリクエストのキャッシュを防ぐ対策
-            $.getJSON( jsonfile + timestamp, function(json) {
+            var commodity = self.attr('datatype');
+            var fileName = 'price_' + commodity + '.json?' + (new Date()).getTime();
+            var url = '/doc/market/' + fileName;
+            $.getJSON(url, function (json) {
                 var items = [];
                 var prices = new Array(json.colsize);
-                $.each(json.prices, function(i, sessions) {
-                    $.each(sessions.values, function(j, value) {
-                        if(value=="") return;
+                $.each(json.prices, function (i, sessions) {
+                    $.each(sessions.values, function (j, value) {
+                        if (value == "") return;
                         prices[j] = value;
                     });
                 });
-                $('#timestamp').append(args.commodity == '55' ? json.timestamp : '');
-                for (var i=0; i < json.colsize; i++) {
+                $('#timestamp').append(commodity == '55' ? json.timestamp : '');
+                for (var i = 0; i < json.colsize; i++) {
                     // 限月一覧 (限月、約定値段(前日比)、出来高)
-                    var durations = json.durations[i] == '' ? '新甫' : json.durations[i]
+                    var durations = json.durations[i] == '' ? '新甫' : json.durations[i];
                     items.push('<li class="list-group-item">');
-                    items.push('<a data-toggle="collapse" href="#collapse' + args.commodity + i + '" aria-controls="collapse' + i + '">');
+                    items.push('<a data-toggle="collapse" href="#collapse' + commodity + i + '">');
                     items.push('<div class="row">');
                     items.push('<div class="col-xs-3 col-sm-2 col-md-2">' + json.months[i] + '</div>');
                     items.push('<div class="col-xs-4 col-sm-3 col-md-3">' + prices[i] + ' (' + durations + ')</div>');
                     items.push('<div class="col-xs-4 col-sm-3 col-md-3">' + json.volumes[i] + '</div>');
-                    items.push('</div>')
-                    items.push('</a>')
+                    items.push('</div>');
+                    items.push('</a>');
 
                     // 詳細テーブル
-                    items.push('<div id="collapse' + args.commodity + i + '" class="collapse panel-body">');
+                    items.push('<div id="collapse' + commodity + i + '" class="collapse panel-body">');
                     items.push('<table class="table">');
                     items.push('<tbody>');
 
-//                    // 1. 限月
-//                    items.push('<tr>');
-//                    items.push('<th colspan=2 class="rowname">限月</th>');
-//                    items.push('<td class="month">'+ esc(json.months[i]) +'</td>');
-//                    items.push('</tr>');
-//
-//                    // 2. 前日終値
-//                    items.push('<tr>');
-//                    items.push('<th colspan=2 class="rowname">前日終値</th>');
-//                    items.push('<td>'+ esc(json.prevprices[i]) +'</td>');
-//                    items.push('</tr>');
-
                     // 3. 約定値段
-                    $.each(json.prices, function(j, sessions) {
+                    $.each(json.prices, function (j, sessions) {
                         items.push('<tr>');
-                        if( json.amsize >= 0 ){
-                            if( 0 == j ){
+                        if (json.amsize >= 0) {
+                            if (0 == j) {
                                 items.push('<th rowspan=' + json.amsize + '>前場</th>');
                             }
                         }
-                        if( json.pmsize >= 0 ){
-                            if( json.amsize == j ){
+                        if (json.pmsize >= 0) {
+                            if (json.amsize == j) {
                                 items.push('<th rowspan=' + json.pmsize + '>後場</th>');
                             }
                         }
                         items.push('<th>' + sessionName(sessions.sid) + '</th>');
-                        items.push('<td >'+ sessions.values[i] +'</td>');
+                        items.push('<td >' + sessions.values[i] + '</td>');
                         items.push('</tr>');
                     });
                     items.push('</tbody>');
@@ -96,33 +60,40 @@ $('li#price-report50').jsonpricereport({
                     items.push('</div>');
                 }
 
-                    // 内容の入れ替え.
-//                    self.contents().remove();
+                // 内容の入れ替え.
+                $('ul', self).remove();
 
-                    $('<ul/>', {
-                        'class': 'list-group',
-                        html: items.join('')
-                    }).appendTo( self );
-
-                })
-                .error(function() {
+                $('<ul/>', {
+                    class: 'list-group',
+                    html: items.join('')
+                }).appendTo(self);
+            })
+                .error(function () {
                     self.contents().remove();
-                    self.append( '<div style="color:red; border: 1px solid red;">[error] データ読込みに失敗しました</div>');
+                    self.append('<div class="text-danger">データ読込みに失敗しました</div>');
                 });
-            });
-
-        // 一応の後始末(元に戻す).
-       $.ajaxSetup({async: true});
+        });
     }
-
 })(jQuery);
 
-function sessionName( sessionid ){
-	if( sessionid == "11" ){ return "第１節"; }
-	if( sessionid == "12" ){ return "第２節"; }
-	if( sessionid == "13" ){ return "第３節"; }
-	if( sessionid == "21" ){ return "第１節"; }
-	if( sessionid == "22" ){ return "第２節"; }
-	if( sessionid == "23" ){ return "第３節"; }
-	return sessionid;
+function sessionName(sessionid) {
+    if (sessionid == "11") {
+        return "第１節";
+    }
+    if (sessionid == "12") {
+        return "第２節";
+    }
+    if (sessionid == "13") {
+        return "第３節";
+    }
+    if (sessionid == "21") {
+        return "第１節";
+    }
+    if (sessionid == "22") {
+        return "第２節";
+    }
+    if (sessionid == "23") {
+        return "第３節";
+    }
+    return sessionid;
 }
